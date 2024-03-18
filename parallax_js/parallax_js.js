2b6056a2bc6be79dbd668794a22c1c707f3622ff
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-19 01:30:03 +0530

    added parallax effects and gsap

--- parallax_js/parallax_js.js
+++ parallax_js/parallax_js.js
@@ -2,16 +2,60 @@ const parallax_el = document.querySelectorAll(".parallax");
 
 let xValue = 0, yValue = 0;
 
+let rotateDegree = 0;
+
+function update(cursorPosition){
+
+    parallax_el.forEach(el => {
+
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+        let speedz = el.dataset.speedz;
+        let rotateSpeed = el.dataset.rotation;
+
+        let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+        let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+
+        // console.log(zValue)
+
+        el.style.transform = 
+        `perspective(2300px) 
+        translateZ(${zValue * speedz}px) 
+        rotateY(${rotateDegree * rotateSpeed}deg)
+        translateX(calc(-50% + ${-xValue * speedx}px)) 
+        translateY(calc(-50% + ${yValue * speedy}px))`;
+    });
+}
+
+update(0);
+
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
-    console.log(xValue, yValue);
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
 
-    parallax_el.forEach(el => {
-        let speedx = el.dataset.speedx;
-        let speedy = el.dataset.speedy;
-        el.style.transform = `translateX(calc(-50% + ${xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`
-    });
+    // console.log(xValue, yValue);
+
+    update(e.clientX);
 
 });
+
+// GSAP Animation
+
+let timeline = gsap.timeline();
+
+Array.from(parallax_el)
+.filter(el => !el.classList.contains("text"))
+.forEach((el) => {
+    timeline.from(
+        el, 
+        {
+            top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+            duration: 3.5,
+        },
+        "1"
+    );
+});
+
+
